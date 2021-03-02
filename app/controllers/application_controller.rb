919939443fb996457c274f95e5b45f78f5a23e3d
chrislemus <icrislemus@gmail.com>
2021-03-02 09:07:58 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, only: :create
+
+  def create
+    pp request.env['omniauth.auth']
+    session[:name] = request.env['omniauth.auth']['info']['name']
+    session[:omniauth_data] = request.env['omniauth.auth']
+    redirect_to root_path
+  end
 end
